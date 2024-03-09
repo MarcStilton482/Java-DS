410a1f161de6978255de184041f45d41891ae6b0
MarcStilton482 <135502520+MarcStilton482@users.noreply.github.com>
2024-03-09 20:22:19 +0530

    Third Commit

--- anagrams/src/Anagrams.java
+++ anagrams/src/Anagrams.java
@@ -1,36 +1,27 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Anagrams {
     public static boolean isAnagram(String first, String second) {
-        char[] charOfFirst = first.replaceAll("\\s", "").toLowerCase().toCharArray();
-        char[] charOfSecond = second.replaceAll("\\s", "").toLowerCase().toCharArray();
-
-        if (charOfFirst.length != charOfSecond.length)
+        first = first.toLowerCase();
+        second = second.toLowerCase();
+        if (first.length() != second.length())
             return false;
-
-        int[] frequencies = new int[26];
-        for (int i = 0; i < charOfFirst.length; i++) {
-            frequencies[charOfFirst[i] - 'a']++;
-            frequencies[charOfSecond[i] - 'a']--;
+        int[] frequencies = new int[256];
+        for (int i = 0; i < first.length(); i++) {
+            frequencies[first.charAt(i)]++;
+            frequencies[second.charAt(i)]--;
         }
-
-        for (int frequency : frequencies) {
-            if (frequency != 0)
-                return false;
-        }
-
-        return true;
+        return Arrays.stream(frequencies).allMatch(frequency -> frequency == 0);
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter First String: ");
         String first = sc.nextLine();
         System.out.println("Enter Second String: ");
         String second = sc.nextLine();
-
         boolean anagram = isAnagram(first, second);
         System.out.println("The given strings are anagrams: " + anagram);
     }
 }
+//Time Complexity : O(n), Space Complexity: O(1)
